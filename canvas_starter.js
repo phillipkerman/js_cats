5f48439f36bc130537de695e948af9478f761bb7
Phillip Kerman <phillipkerman@gmail.com>
2013-02-19 14:53:20 -0800

    using ctx now, not c in canvas_starter.js

--- canvas_starter.js
+++ canvas_starter.js
@@ -19,14 +19,14 @@ var ctx = (function(){
 
 drawOne = function (index){
 	var width=30, space=3;
-	c.fillRect(index*(width+space), 0, width, width);
+	ctx.fillRect(index*(width+space), 0, width, width);
 }
 
 
 
 var listOfObjects = []
 update = function(){
-	c.clearRect(0, 0, c.canvas.width, c.canvas.height);
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 	var total = listOfObjects.length;
 	for(var i=0; i<total; i++){
 		listOfObjects[i].update();
@@ -34,8 +34,8 @@ update = function(){
 }
 
 var obj = function(){
-	this.x = Math.random()*c.canvas.width;
-	this.y = Math.random()*c.canvas.height;
+	this.x = Math.random()*ctx.canvas.width;
+	this.y = Math.random()*ctx.canvas.height;
 	this.dirX = 1-Math.random()*2;
 	this.dirY = 1-Math.random()*2;
 }
@@ -43,9 +43,9 @@ var obj = function(){
 obj.prototype.update =function(){
 		this.x += this.dirX;
 		this.y += this.dirY;
-		if ( this.x > c.canvas.width ) this.x = 0;
-		if ( this.y > c.canvas.height ) this.y = 0;
-		c.fillRect(this.x, this.y, 10, 10);
+		if ( this.x > ctx.canvas.width ) this.x = 0;
+		if ( this.y > ctx.canvas.height ) this.y = 0;
+		ctx.fillRect(this.x, this.y, 10, 10);
 	}
 
 for(var i=0; i<100; i++){
